df8d3d0f3a0635cf722542391158d40b582cf97d
Jamie Kuesel <jamiekuesel@MacBook-Air-3.local>
2022-06-26 22:43:07 +0200

    Forget to add home.js

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,14 +2,17 @@ import React from "react";
 
 import Bio from "./HomeComponents/Bio/Bio";
 import Skills from "./HomeComponents/Skills/Skills";
+import UnderConstruction from "./HomeComponents/UnderConstruction/UnderConstruction";
 import RecentProjects from "./HomeComponents/RecentProjects/RecentProjects";
 
 import "./Home.scss"
+
 
 const Home = () => {
 
     return (
         <div className="home__wrapper">
+            <UnderConstruction/>
             <Bio/>
             <Skills/>
             <RecentProjects/>
