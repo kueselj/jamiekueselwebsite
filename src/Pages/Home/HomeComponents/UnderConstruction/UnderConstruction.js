dd04d8914dbaa6152119d94e213a2ba318c3f19c
Jamie Kuesel <jamiekuesel@MacBook-Air-3.local>
2022-06-26 22:17:45 +0200

    Adding under construction message component

---
+++ src/Pages/Home/HomeComponents/UnderConstruction/UnderConstruction.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./UnderConstruction.scss"
+
+const UnderConstruction = () => {
+
+    return (
+        <div className = 'under__wrapper'> 
+            Greetings! This website is still under construction and will be for some time! I still have a plethora of projects to add and am somewhat stuck in CSS purgatory! Neat, abstract, markdown project posts coming soon!
+        </div>
+    )
+}
+
+export default UnderConstruction;
